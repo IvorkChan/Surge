13f39434bc4b8c6e2ec7475388780d1d68ec54eb
Chan Ivork <Arvonik@gmail.com>
2020-07-02 13:58:27 +0800

    Add account

--- Script/BilibiliAd.js
+++ Script/BilibiliAd.js
@@ -82,5 +82,25 @@ if (-1 != $request.url.indexOf('resource/show/tab?') && 0 == body['code']) {
     });
 }
 
+// 我的
+if (-1 != $request.url.indexOf('/x/v2/account/mine') && 0 == body['code']) {
+    body['data']['vip_section'] = {};
+    body['data']['vip_section_v2'] = {};
+    body['data']['sections'] = body['data']['sections'].filter(
+        function (item, index) {
+            if ('创作中心' == item.title) {
+                return false;
+            }
+            item.items = item.items.filter(function (section_items) {
+                console.log(section_items.title);
+                if ((['我的关注', '我的钱包', '会员购中心', '直播中心', '青少年模式', '看视频免流量'].includes(section_items.title))) {
+                    return false;
+                }
+                return true;
+            });
+            return true;
+        });
+}
+
 body = JSON.stringify(body);
 $done({body});
