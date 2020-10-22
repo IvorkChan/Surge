2107a4e0ca3b8ef59808397d89b759b36120524d
Chan Ivork <Arvonik@gmail.com>
2020-10-22 13:05:56 +0800

    Update

--- Script/Airmail.js
+++ Script/Airmail.js
@@ -1,30 +1,21 @@
-let obj=JSON.parse($response.body);
-let url=$request.url;
-
-if(url.endsWith("offerings")||url.endsWith("products")) {
-	$done({});
-} else {
-	if (url.indexOf("5647911E-B243-48C9-A25C-29116412A20D")!=-1) {
-		obj["subscriber"]["entitlements"]={
-			"Airmail Premium": {
-				"expires_date": "2099-12-01T00:00:00Z",
-				"product_identifier": "Airmail_iOS_Yearly",
-				"purchase_date": "2019-12-01T00:00:00Z"
-			}
-		};
-		obj["subscriber"]["subscriptions"]={
-			"Airmail_iOS_Yearly": {
-				"is_sandbox": false,
-				"period_type": "active",
-				"billing_issues_detected_at": null,
-				"unsubscribe_detected_at": null,
-				"expires_date": "2099-12-01T00:00:00Z",
-				"original_purchase_date": "2019-10-31T00:00:00Z",
-				"purchase_date": "2019-10-31T00:00:00Z",
-				"store": "app_store"
-			}
-		};
-	}
-}
-
-$done({body:JSON.stringify(obj)});
+let obj = JSON.parse($response.body);
+obj["subscriber"]["subscriptions"]= {
+      "revenuecat.pro.yearly": {
+        "is_sandbox": false,
+        "period_type": "active",
+        "billing_issues_detected_at": null,
+        "unsubscribe_detected_at": null,
+        "expires_date": "2099-12-01T03:51:32Z",
+        "original_purchase_date": "2019-10-31T02:51:33Z",
+        "purchase_date": "2019-10-31T02:51:32Z",
+        "store": "app_store"
+      }
+    };
+obj["subscriber"]["entitlements"]= {
+      "pro": {
+        "expires_date": "2099-12-01T03:51:32Z",
+        "product_identifier": "revenuecat.pro.yearly",
+        "purchase_date": "2019-10-31T02:51:32Z"
+      }
+    };
+$done({body: JSON.stringify(obj)});
