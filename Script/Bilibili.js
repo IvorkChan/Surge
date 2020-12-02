40c04a2a794fbe7c75cfeb03e01866d71bc068a5
IvorkChan <arvonik@gmail.com>
2020-12-02 19:04:54 +0800

    Update Bilibili.js

--- Script/Bilibili.js
+++ Script/Bilibili.js
@@ -81,5 +81,25 @@ if (-1 != $request.url.indexOf('resource/show/tab?') && 0 == body['code']) {
         return item.id != 222});
 }
 
+// 我的
+if (-1 != $request.url.indexOf('/x/v2/account/mine') && 0 == body['code']) {
+    body['data']['vip_section'] = {};
+    body['data']['vip_section_v2'] = {};
+    body['data']['sections_v2'] = body['data']['sections_v2'].filter(
+        function (item, index) {
+            if (['创作中心', '创作首页', '创作学院', '打卡挑战', '有奖活动'].includes(item.title)) {
+                return false;
+            }
+            item.items = item.items.filter(function (section_v2_items) {
+                console.log(section_items.title);
+                if ((['推荐服务', '我的课程', '看视频免流量', '个性装扮', '邀好友赚红包', '游戏中心', '我的钱包', '会员购中心', '直播中心', '反馈论坛', '充能领福利', '课堂模式', '青少年模式'].includes(section_items.title))) {
+                    return false;
+                }
+                return true;
+            });
+            return true;
+        });
+}
+
 body = JSON.stringify(body);
 $done({body});
