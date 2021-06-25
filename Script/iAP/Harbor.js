dee3253d88f44e73aa820440b6e36e077f23562c
IvorkChan <arvonik@gmail.com>
2021-06-26 00:49:37 +0800

    Update Harbor.js

--- Script/iAP/Harbor.js
+++ Script/iAP/Harbor.js
@@ -4,8 +4,11 @@ obj["subscriber"]["subscriptions"]= {
         "is_sandbox": true,
         "ownership_type": "PURCHASED",
         "billing_issues_detected_at": null,
+        "period_type" : "normal",
         "unsubscribe_detected_at": null,
         "expires_date": "2050-01-01T00:00:00Z",
+        "grace_period_expires_date" : null,
+        "unsubscribe_detected_at" : null,
         "original_purchase_date": "2020-01-01T00:00:00Z",
         "purchase_date": "2020-01-01T00:00:00Z",
         "store": "app_store"
@@ -13,9 +16,10 @@ obj["subscriber"]["subscriptions"]= {
     };
 obj["subscriber"]["entitlements"]= {
       "premium": {
-        "expires_date": "2050-01-01T00:00:00Z",
-        "product_identifier": "revenuecat.pro.yearly",
+        "grace_period_expires_date" : null,
         "purchase_date": "2020-01-01T00:00:00Z"
+        "product_identifier": "io.pixelfoundry.Harbor.yearly",
+        "expires_date": "2050-01-01T00:00:00Z",
       }
     };
 $done({body: JSON.stringify(obj)});
