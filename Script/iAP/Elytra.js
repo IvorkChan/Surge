b62daa1c56ea86b4e16ead1259da49d30304bc73
IvorkChan <arvonik@gmail.com>
2021-06-26 12:36:10 +0800

    Update Elytra.js

--- Script/iAP/Elytra.js
+++ Script/iAP/Elytra.js
@@ -4,13 +4,13 @@ obj =
   "status" : true,
   "subscription" : {
     "stripe" : null,
-    "expiry" : null,
-    "status" : 0,
-    "id" : 5357,
-    "originalTransactionID" : "1000000832914633",
+    "expiry" : "2050-01-01T00:00:00Z",
+    "status" : 1,
+    "id" : 5360,
+    "userID": "8780",
+    "originalTransactionID" : null,
     "environment" : "ProductionSandbox",
     "created" : "2020-01-01T00:00:00Z",
-    "preAppstore" : false
   }
 }
 $done({body: JSON.stringify(obj)});
